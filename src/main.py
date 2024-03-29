6e8f301cbddec81cbe491007c957d21be0afdb66
Jonny Stendahl <jonny.stendahl@skjulet.se>
2024-03-29 20:48:24 +0100

    Fixes

--- src/main.py
+++ src/main.py
@@ -10,13 +10,13 @@ def load_yaml(file_path):
 def generate_create_table_sql(tables):
     env = Environment(loader=FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
     template = env.get_template('create_table.sql')
-    return template.render(tables=tables)
+
+    for table in tables:
+        print(template.render(table=table))
+        print('---------')
 
 if __name__ == "__main__":
     tables_yaml_path = "./metadata/tables.yaml"
 
     tables_data = load_yaml(tables_yaml_path)
-    create_table_sql = generate_create_table_sql(tables_data['tables'])
-
-    # Print the generated SQL to the console
-    print(create_table_sql)
+    generate_create_table_sql(tables_data['tables'])
