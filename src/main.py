d338c39e6a78e4b48269e0876a814f1f95a155a3
Jonny Stendahl <jonny.stendahl@skjulet.se>
2024-03-31 23:42:22 +0200

    Fix

--- src/main.py
+++ src/main.py
@@ -30,18 +30,23 @@ def generate_create_table_sql(tables):
         create_table_ddl = template.render(table=table)
         write_statement_to_files(folder, subfolder, filename, create_table_ddl)
 
-def generate_hubs_sql(tables):
+def generate_hubs_sql():
     template = env.get_template('load_hubs.jinja2')
+    hubs_yaml_path = "./metadata/hubs.yaml"
+
+    hubs_data = load_yaml(hubs_yaml_path)
     
-    for table in tables:
-        folder = table['folder']
-        subfolder = table['subfolder']
-        filename = "create_{}.sql".format(table['name'])
-        create_table_ddl = template.render(table=table)
-        write_statement_to_files(folder, subfolder, filename, create_table_ddl)
+    for hub in hubs_data['hubs']:
+        folder = hub['folder']
+        subfolder = hub['subfolder']
+        filename = "ETL_{}.sql".format(hub['name'])
+        hub_merge_sql = template.render(hub=hub)
+        write_statement_to_files(folder, subfolder, filename, hub_merge_sql)
 
 if __name__ == "__main__":
     tables_yaml_path = "./metadata/tables.yaml"
 
     tables_data = load_yaml(tables_yaml_path)
     generate_create_table_sql(tables_data['tables'])
+
+    generate_hubs_sql()
