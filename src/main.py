8edce234deb8ff7b41e25473f71968d2889d8855
Jonny Stendahl <jonny.stendahl@skjulet.se>
2024-03-31 22:07:11 +0200

    fix

--- src/main.py
+++ src/main.py
@@ -2,6 +2,16 @@ import os
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
+def write_table_to_files(folder, subfolder, filename, content):
+    # Create directory if it doesn't exist
+    directory = os.path.join(folder, subfolder)
+    os.makedirs(directory, exist_ok=True)
+
+    # Write table to file
+    filepath = os.path.join(directory, filename)
+    with open(filepath, 'w') as f:
+        f.write(content)
+            
 def load_yaml(file_path):
     with open(file_path, 'r') as file:
         data = yaml.safe_load(file)
@@ -9,11 +19,14 @@ def load_yaml(file_path):
 
 def generate_create_table_sql(tables):
     env = Environment(loader=FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
-    template = env.get_template('create_table.sql')
-
+    template = env.get_template('create_table.jinja2')
+    
     for table in tables:
-        print(template.render(table=table))
-        print('---------')
+        folder = table['folder']
+        subfolder = table['subfolder']
+        filename = "create_{}.sql".format(table['name'])
+        create_table_ddl = template.render(table=table)
+        write_table_to_files(folder, subfolder, filename, create_table_ddl)
 
 if __name__ == "__main__":
     tables_yaml_path = "./metadata/tables.yaml"
